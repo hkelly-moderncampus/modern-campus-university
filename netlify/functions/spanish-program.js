26ddfe23bbfc488feb1f21185d88bfcc88f7b33c
hkelly-moderncampus <hkelly@moderncampus.com>
2026-05-26 14:59:55 -0400

    Update spanish-program.js

--- netlify/functions/spanish-program.js
+++ netlify/functions/spanish-program.js
@@ -2,28 +2,40 @@ exports.handler = async () => {
   const API_KEY = process.env.ACALOG_API_KEY;
 
   const admissionsUrl =
-    `https://apis.acalog.com/v1/content?format=xml&key=${API_KEY}&catalog=64&method=getItems&type=programs&ids[]=16583&options[full]=1`;
+    `https://apis.acalog.com/v1/content?format=xml&key=${API_KEY}&catalog=64&method=getItems&type=programs&ids%5B%5D=16583&options%5Bfull%5D=1`;
 
   const degreeUrl =
-    `https://apis.acalog.com/v1/content?format=xml&key=${API_KEY}&catalog=64&method=getItems&type=programs&ids[]=15711&options[full]=1`;
+    `https://apis.acalog.com/v1/content?format=xml&key=${API_KEY}&catalog=64&method=getItems&type=programs&ids%5B%5D=15711&options%5Bfull%5D=1`;
 
-  const [admissionsRes, degreeRes] = await Promise.all([
-    fetch(admissionsUrl),
-    fetch(degreeUrl)
-  ]);
+  try {
+    const [admissionsRes, degreeRes] = await Promise.all([
+      fetch(admissionsUrl),
+      fetch(degreeUrl)
+    ]);
 
-  const admissionsXml = await admissionsRes.text();
-  const degreeXml = await degreeRes.text();
+    const admissionsXml = await admissionsRes.text();
+    const degreeXml = await degreeRes.text();
 
-  return {
-    statusCode: 200,
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*"
-    },
-    body: JSON.stringify({
-      admissions: admissionsXml,
-      degree: degreeXml
-    })
-  };
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+      },
+      body: JSON.stringify({
+        admissions: admissionsXml,
+        degree: degreeXml
+      })
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        error: error.message
+      })
+    };
+  }
 };
